629dfe088b5677e4ca4d116e77f8ee7297942ae2
Ramiro <haly5@hotmail.com>
2014-06-08 20:46:17 -0300

    Added a stub User show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   root  'pages#home'
+
+  resources :users
+
   match '/help',    to: 'pages#help',    via: 'get'
   match '/about',   to: 'pages#about',   via: 'get'
   match '/contact', to: 'pages#contact', via: 'get'
