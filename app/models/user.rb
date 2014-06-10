33a17efbc6d61243f333254fbc33907e47548e96
Ramiro <haly5@hotmail.com>
2014-06-10 20:46:28 -0300

    Added password and encrypted_password attributes to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,17 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
+
+	attr_accessor	:password
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -18,5 +21,8 @@ class User < ActiveRecord::Base
 	validates :email, 	presence: true,
 						format: {with: email_regex},
 						uniqueness: {case_sensitive: false}
+	validates :password,	presence: true,
+							confirmation: true,
+							length: {within: 6..40} 
 
 end
