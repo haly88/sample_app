ca54152c4da055942a855417d6214b7a4d0a4ec0
Ramiro <haly5@hotmail.com>
2014-06-16 01:23:51 -0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
 	def show
-  	@user = User.find(params[:id])
   	@title = @user.name
   end
   
@@ -10,4 +11,24 @@ class UsersController < ApplicationController
   	@title = "Sign up"
   end
 
+  def create
+  	@user = User.new(user_params)
+      if @user.save
+        redirect_to @user, flash: {success: "Welcome to the Sample App!"}
+      else
+  	   @title = "Sign up"
+  	   render "new"
+     end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
